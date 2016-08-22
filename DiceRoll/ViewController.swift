8189ee4c3f443c92657e376f3c8427b429676057
master <libaoming2@gmail.com>
2016-08-22 21:14:57 +0800

    complete diceroll

--- DiceRoll/ViewController.swift
+++ DiceRoll/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
+    @IBAction func guessTapped(_ sender: AnyObject) {
+        
+        let diceRoll = String(arc4random_uniform(6))
+        
+        if let text = textField.text {
+                if text == diceRoll {
+                    resultLabel.text = "you are right"
+                }else {
+                    resultLabel.text = "the number is \(diceRoll)"
+                }
+                
+            }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
